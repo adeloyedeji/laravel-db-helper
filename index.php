99b20b3a88144b9e0deea132eb0d747f4dc19457
primuszeus <6009197+adeloyedeji@users.noreply.github.com>
2019-09-17 07:32:20 +0100

    Edited config file

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require __DIR__ . '/vendor/autoload.php';
 use App\DbToModel;
 use App\setup\Inflector;
 
-$obj = new DbToModel("localhost", "root", "", "ipos_current");
+$obj = new DbToModel("localhost", "root", "", "testdb");
 
 $result = $obj->getDatabaseTables();
 
