17313214fb33d52bbfa792965a223912e6c57d31
ibro <street@localhost.localdomain>
2020-09-15 10:39:01 +0000

    added support for database

--- index.php
+++ index.php
@@ -6,15 +6,15 @@ use App\setup\Inflector;
 
 $val = getopt("s:m:h");
 
-$database = isset(getopt(null, ["database:"])['database'])? getopt(null, ["database:"])['database'] : NULL;
+$database = isset(getopt(null, ["database:"])['database']) ? getopt(null, ["database:"])['database'] : NULL;
 
-$password = isset(getopt(null, ["password:"])['password'])? getopt(null, ["password:"])['password'] : "";
+$password = isset(getopt(null, ["password:"])['password']) ? getopt(null, ["password:"])['password'] : "";
 
-$host = isset(getopt(null, ["host:"])['host'])? getopt(null, ["host:"])['host'] : "localhost";
+$host = isset(getopt(null, ["host:"])['host']) ? getopt(null, ["host:"])['host'] : "localhost";
 
-$username = isset(getopt(null, ["username:"])['username'])? getopt(null, ["username:"])['username'] : NULL;
+$username = isset(getopt(null, ["username:"])['username']) ? getopt(null, ["username:"])['username'] : NULL;
 
-$path = isset(getopt(null, ["path:"])['path'])? getopt(null, ["path:"])['path']: ".";
+$path = isset(getopt(null, ["path:"])['path']) ? getopt(null, ["path:"])['path'] : ".";
 
 
 $welcomeNote  = "********************************************\n";
@@ -39,10 +39,10 @@ $welcomeNote .= "\t-seeder -model \n\n";
 $welcomeNote .= "usage: php index.php -help, to display help\n";
 $welcomeNote .= "********************************************\n";
 
-if($val == [] || !isset($username) || !isset($database) ) {
+if ($val == [] || !isset($username) || !isset($database)) {
 
     echo $welcomeNote;
-die();
+    die();
 }
 $start = microtime();
 
@@ -54,185 +54,158 @@ $obj = new DbManager($host, $username, $password, $database);
 $result = $obj->getDatabaseTables();
 
 
-if (count($result) > 0)
-{
-    if($val['m'] == "odels") {
-        if (is_dir(__DIR__."models"))
-        {
+if (count($result) > 0) {
+    if ($val['m'] == "odels") {
+        if (is_dir(__DIR__ . "models")) {
             $dir = 'models';
             $it = new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS);
             $files = new RecursiveIteratorIterator($it, RecursiveIteratorIterator::CHILD_FIRST);
-            foreach($files as $file) {
+            foreach ($files as $file) {
                 if ($file->isDir()) {
                     rmdir($file->getRealPath());
                 } else {
                     unlink($file->getRealPath());
                 }
             }
-            rmdir(__DIR__.$dir);
+            rmdir(__DIR__ . $dir);
         }
         $dir = $path . "/models";
         mkdir($dir, 0777, true);
-        foreach ($result as $r)
-        {
-            foreach($r as $o => $v)
-            {
+        foreach ($result as $r) {
+            foreach ($r as $o => $v) {
                 $columns = $obj->getColumnsInTable($v);
-                if (count($columns) > 0)
-                {
+                if (count($columns) > 0) {
                     // create the model file
                     $laravelModelName = Inflector::singularize(str_replace(" ", "", ucwords(str_replace("_", " ", $v))));
-                    
+
                     // create the file content
                     $modelFileContent = $obj->writeFileOpeningProperties($laravelModelName, $v);
-                    foreach($columns as $c => $v)
-                    {
+                    foreach ($columns as $c => $v) {
                         $modelFileContent .= "\t\t'" . $v['Field'] . "', \n";
                     }
                     $modelFileContent .= $obj->writeFileClosingProperties();
 
                     // write the file content to folder models.
-                    if (file_put_contents($dir."/".$laravelModelName.".php", $modelFileContent) !== false)
-                    {
+                    if (file_put_contents($dir . "/" . $laravelModelName . ".php", $modelFileContent) !== false) {
                         echo "Model: $laravelModelName was successfully created.\n";
-                    }
-                    else 
-                    {
+                    } else {
                         echo "Unable to create model: $laravelModelName.\n";
                     }
                 }
             }
         }
     }
-    
 
 
-   
-    if($val['s'] == "eeders") {
+
+
+    if ($val['s'] == "eeders") {
         //generate seeders;
-        if (is_dir(__DIR__."seeders"))
-        {
+        if (is_dir(__DIR__ . "seeders")) {
             $dir = 'seeders';
             $it = new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS);
             $files = new RecursiveIteratorIterator($it, RecursiveIteratorIterator::CHILD_FIRST);
-            foreach($files as $file) {
+            foreach ($files as $file) {
                 if ($file->isDir()) {
                     rmdir($file->getRealPath());
                 } else {
                     unlink($file->getRealPath());
                 }
             }
-            rmdir(__DIR__.$dir);
+            rmdir(__DIR__ . $dir);
         }
         $dir = $path . "/seeders";
         mkdir($dir, 0777, true);
 
         $baseSeederName = "DatabaseSeeder";
         $baseSeederContent = "";
-        foreach ($result as $table)
-        {
-     
-            foreach($table as $key => $tableName)
-            {
-            
+        foreach ($result as $table) {
+
+            foreach ($table as $key => $tableName) {
+
                 $columns = $obj->getColumnsInTable($tableName);
                 $tableData = $obj->getTableData($tableName);
                 $className =  str_replace(" ", "", ucwords(str_replace("_", " ", $tableName)));
 
-                if (count($columns) > 0)
-                {
+                if (count($columns) > 0) {
                     $columnField = array();
-                    foreach($columns as $column) {
-                    $columnField[] = $column['Field'];
-                    
+                    foreach ($columns as $column) {
+                        $columnField[] = $column['Field'];
                     }
-                
+
                     $i = 0;
-                    $content="";
-                    foreach($tableData as $data) {
+                    $content = "";
+                    foreach ($tableData as $data) {
                         $content .= "\n\t\t\t[\n";
-                    foreach($columnField as $singleColumn) {
-                            $content .= "\t\t\t\t'".$singleColumn."' => '".$data[$i]."',\n";
+                        foreach ($columnField as $singleColumn) {
+                            $content .= "\t\t\t\t'" . $singleColumn . "' => '" . $data[$i] . "',\n";
                             $i++;
-                            if($i == count($columnField)) $i = 0;
+                            if ($i == count($columnField)) $i = 0;
+                        }
+                        $content .= "\n\t\t\t],\n";
                     }
-                    $content .= "\n\t\t\t],\n";
-                    
-                    
-                    }  
-                    if($content != "")$content = $obj->writeSeederProperties($tableName,$content,$className);
-                    if (file_put_contents($dir."/".$className."TableSeeder.php", $content) !== false)
-                    {
+                    if ($content != "") $content = $obj->writeSeederProperties($tableName, $content, $className);
+                    if (file_put_contents($dir . "/" . $className . "TableSeeder.php", $content) !== false) {
                         $baseSeederContent .= "\t\t\$this->call({$className}TableSeeder::class);\n";
-                        echo "Seeders: ".$className. "TableSeeder was successfully created.\n";
-                    }
-                    else 
-                    {
+                        echo "Seeders: " . $className . "TableSeeder was successfully created.\n";
+                    } else {
                         echo "Unable to create model: $laravelModelName.\n";
-                    }             
+                    }
                 }
             }
-          
         }
-        $file = $obj->writeDatabaseSeederFile($baseSeederContent,$baseSeederName);
-        if (file_put_contents($dir."/".$baseSeederName.".php", $file) !== false)
-                    {
-                       
-                        echo "Seeders: ".$baseSeederName. " was successfully created.\n";
-                    }
-                    else 
-                    {
-                        echo "Unable to create model: $laravelModelName.\n";
-                    }     
+        $file = $obj->writeDatabaseSeederFile($baseSeederContent, $baseSeederName);
+        if (file_put_contents($dir . "/" . $baseSeederName . ".php", $file) !== false) {
+
+            echo "Seeders: " . $baseSeederName . " was successfully created.\n";
+        } else {
+            echo "Unable to create model: $laravelModelName.\n";
+        }
     }
 
-    if($val['m'] == "igrations") {
-        if (is_dir(__DIR__."migrations"))
-        {
+    if ($val['m'] == "igrations") {
+        if (is_dir(__DIR__ . "migrations")) {
             $dir = 'migrations';
             $it = new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS);
             $files = new RecursiveIteratorIterator($it, RecursiveIteratorIterator::CHILD_FIRST);
-            foreach($files as $file) {
+            foreach ($files as $file) {
                 if ($file->isDir()) {
                     rmdir($file->getRealPath());
                 } else {
                     unlink($file->getRealPath());
                 }
             }
-            rmdir(__DIR__.$dir);
+            rmdir(__DIR__ . $dir);
         }
         $dir = $path . "/migrations";
         mkdir($dir, 0777, true);
-        foreach ($result as $r)
-        {
-         
-            foreach($r as $o => $v)
-            {
-                
-                
-             
+        foreach ($result as $r) {
+
+            foreach ($r as $o => $v) {
+
+
+
                 $columns = $obj->getColumnsInTable($v);
 
-                               
-                if (count($columns) > 0)
-                {
+
+                if (count($columns) > 0) {
                     $time = time();
-                    $date = date("Y_m_d",time());
+                    $date = date("Y_m_d", time());
                     $time = $time % 10000;
 
-                   
 
-                    
+
+
                     $migrationFileName = "{$date}_{$time}_create_{$v}_table.php";
-                    
-                    $className = "Create".str_replace(" ", "", ucwords(str_replace("_", " ", $v)))."Table";
 
-                    
+                    $className = "Create" . str_replace(" ", "", ucwords(str_replace("_", " ", $v))) . "Table";
+
+
                     $upContents = "";
                     $upContents .= "\t\tSchema::create('{$v}', function (Blueprint \$table) {\n";
-                    
+
                     $hasTimeStamp = false;
-                    foreach($columns as $column) {
+                    foreach ($columns as $column) {
                         $field = $column["Field"];
                         $type = $column["Type"];
                         $nullable = $column["Null"];
@@ -240,101 +213,93 @@ if (count($result) > 0)
                         $defaultValue = $column["Default"];
                         $extra = $column["Extra"];
 
-                        
+
                         $columnString = "\t\t\t\$table";
-                        if($field == "id") {
+                        if ($field == "id") {
                             $columnString .= "->id()";
-                        }elseif($field == "created_at" || $field == "updated_at"){
-                            if(!$hasTimeStamp){
+                        } elseif ($field == "created_at" || $field == "updated_at") {
+                            if (!$hasTimeStamp) {
                                 $columnString .= "->timestamps()";
-                                $hasTimeStamp =true;
-                            }else {
-                              
+                                $hasTimeStamp = true;
+                            } else {
+
                                 continue;
                             }
-                            
-                           
-                        }else {
-                            
-                            if(strpos($type,"bigint") !== false) {
+                        } else {
+
+                            if (strpos($type, "bigint") !== false) {
                                 $columnString .= "->bigInteger('{$field}')";
-                               
-                            }elseif(strpos($type,"mediumint") !== false) {
+                            } elseif (strpos($type, "mediumint") !== false) {
                                 $columnString .= "->mediumInteger('{$field}')";
-                            }elseif(strpos($type,"varchar") !== false) {
+                            } elseif (strpos($type, "varchar") !== false) {
                                 $columnString .= "->string('{$field}')";
-                            }elseif(strpos($type,"longtext") !== false) {
+                            } elseif (strpos($type, "longtext") !== false) {
                                 $columnString .= "->longText('{$field}')";
-                            }elseif(strpos($type,"smallint") !== false) {
+                            } elseif (strpos($type, "smallint") !== false) {
                                 $columnString .= "->smallInteger('{$field}')";
-                            }elseif(strpos($type,"tinyint") !== false) {
+                            } elseif (strpos($type, "tinyint") !== false) {
                                 $columnString .= "->tinyInteger('{$field}')";
-                            }elseif(strpos($type,"timestamp") !== false) {
+                            } elseif (strpos($type, "timestamp") !== false) {
                                 $columnString .= "->timestamp('{$field}')";
-                            }elseif(strpos($type,"time") !== false) {
+                            } elseif (strpos($type, "time") !== false) {
                                 $columnString .= "->time('{$field}')";
-                            }elseif(strpos($type,"mediumtext") !== false) {
+                            } elseif (strpos($type, "mediumtext") !== false) {
                                 $columnString .= "->mediumText('{$field}')";
-                            }elseif(strpos($type,"text") !== false) {
+                            } elseif (strpos($type, "text") !== false) {
                                 $columnString .= "->text('{$field}')";
-                            }elseif(strpos($type,"int") !== false) {
+                            } elseif (strpos($type, "int") !== false) {
                                 $columnString .= "->integer('{$field}')";
-                            }elseif(strpos($type,"boolean") !== false){
+                            } elseif (strpos($type, "boolean") !== false) {
                                 $columnString .= "->boolean('{$field}')";
-                            }elseif(strpos($type,"char") !== false){
+                            } elseif (strpos($type, "char") !== false) {
                                 $columnString .= "->char('{$field}')";
-                            }elseif(strpos($type,"double") !== false) {
+                            } elseif (strpos($type, "double") !== false) {
                                 $columnString .= "->double('{$field}')";
-                            }elseif(strpos($type,"float") !== false) {
+                            } elseif (strpos($type, "float") !== false) {
                                 $columnString .= "->float('{$field}')";
-                            }elseif(strpos($type,"blob") !== false) {
+                            } elseif (strpos($type, "blob") !== false) {
                                 $columnString .= "->binary('{$field}')";
-                            }elseif(strpos($type,"date") !== false) {
+                            } elseif (strpos($type, "date") !== false) {
                                 $columnString .= "->date('{$field}')";
-                            }elseif(strpos($type,"year") !== false) {
+                            } elseif (strpos($type, "year") !== false) {
                                 $columnString .= "->year('{$field}')";
-                            }elseif(strpos($type,"decimal") !== false) {
+                            } elseif (strpos($type, "decimal") !== false) {
                                 $columnString .= "->decimal('{$field}')";
-                            }else {
+                            } else {
                                 throw new Exception("unknow type {$type}");
                             }
 
-                            if(strpos($type,"unsigned") !== false) {
+                            if (strpos($type, "unsigned") !== false) {
                                 $columnString .= "->unsigned()";
                             }
-                           
-                            if($nullable == "YES" || $nullable == "yes") {
+
+                            if ($nullable == "YES" || $nullable == "yes") {
                                 $columnString .= "->nullable()";
                             }
 
-                            if(strlen($defaultValue) > 0 ) {
+                            if (strlen($defaultValue) > 0) {
                                 $columnString .= "->default({$defaultValue})";
                             }
                         }
 
                         $columnString .= ";\n";
 
-                       $upContents .= $columnString;
-                       
-                        
+                        $upContents .= $columnString;
                     }
                     $upContents .= "\t\t});\n";
 
-                  
-                    $downContenet = "\t\tSchema::dropIfExists('{$v}');\n";
-                    
-                    // create the file content
-                    $migrationFileContent = $obj->writeMigrationsProperties($upContents,$className,$downContenet);
 
-                   
+                    $downContenet = "\t\tSchema::dropIfExists('{$v}');\n";
+
+                    // create the file content
+                    $migrationFileContent = $obj->writeMigrationsProperties($upContents, $className, $downContenet);
+
+
                     // write the file content to folder migrations.
 
-                    if (file_put_contents($dir."/".$migrationFileName, $migrationFileContent) !== false)
-                    {
+                    if (file_put_contents($dir . "/" . $migrationFileName, $migrationFileContent) !== false) {
                         echo "Migration: $migrationFileName was successfully created.\n";
-                    }
-                    else 
-                    {
+                    } else {
                         echo "Unable to create migration: $migrationFileName.\n";
                     }
                 }
@@ -342,15 +307,11 @@ if (count($result) > 0)
         }
     }
 
-    if($val['h'] == "elp") {
+    if ($val['h'] == "elp") {
         echo $welcomeNote;
         die();
     }
-
-    
-}
-else 
-{
+} else {
     echo "Seems like this is a new database with no tables :)";
 }
 echo "Done. Program ran successfully in " . ($start - microtime()) . "milliseconds.\n";
